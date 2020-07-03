65a9f64e1dffd13293e6f0c01fd9ab55cfed7798
jannalari <66527740+jannalari@users.noreply.github.com>
2020-07-03 20:29:23 +0300

    :)

---
+++ MarioParty/MarioParty/models/player.swift
@@ -0,0 +1,31 @@
+//
+//  player.swift
+//  MarioParty
+//
+//  Created by party time on 7/3/20.
+//  Copyright © 2020 Dalal Alhazeem. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var name: String
+   func bgName() -> String
+   {
+      return name +  "BG"
+    }
+    func musicName() -> String {
+        return name + ".WAV"
+    }
+}
+    
+let players : [Player] = [
+    
+  Player(name: "Mario"),
+    Player(name: "Bowser"),
+    Player(name: "Luigi"),
+     Player(name: "Waluigi"),
+    Player(name: "Peach"),
+    Player(name: "Yoshi")
+]
+
